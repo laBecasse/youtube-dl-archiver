16dc94fdb0aadd05dcecb78e52b4e06736f94976
La Bécase <maxime@buron.coffee>
2019-08-22 15:42:55 +0200

    update

--- front/public/sw.js
+++ front/public/sw.js
@@ -1,6 +1,6 @@
 console.log('------------------------- Hello from sw.js --------------------')
 
-const version = 'v0.03'
+const version = 'v0.04'
 const staticCacheName = 'staticfiles' + version
 const imagesCacheName = 'images'
 
