fe64e023107e4821679c181326f529fda379358d
La Bécase <maxime@buron.coffee>
2019-11-23 18:24:23 +0100

    attempt for dark theme

---
+++ front/src/components/Settings.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox" v-model="darkMode" v-on:change="emitSettings">
+      Dark Mode
+    </label>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    emitSettings () {
+        this.$store.commit('setSettings', this)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
