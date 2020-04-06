624f7d7a9cd9093af52fcd5014445b546a251879
La Bécase <maxime@buron.coffee>
2020-04-06 19:27:10 +0200

    sw upgrade

--- front/public/sw.js
+++ front/public/sw.js
@@ -1,6 +1,6 @@
 console.log('------------------------- Hello from sw.js --------------------')
 
-const version = 'v0.12'
+const version = 'v0.13'
 const staticCacheName = 'staticfiles' + version
 const imagesCacheName = 'images'
 
