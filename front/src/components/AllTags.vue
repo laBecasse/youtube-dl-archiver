8eeeb2270ff85477772b9b3933b3560ae3bcb542
La Bécase <maxime@buron.coffee>
2020-05-25 14:00:11 +0200

    all tags page

--- front/src/components/AllTags.vue
+++ front/src/components/AllTags.vue
@@ -1,24 +1,46 @@
 <template>
-    <Tags :tags="tags" :limited="false" :enabledRemoving="false" />
+    <div>
+        <h1 class="is-2 title">{{tags.length}} Tags</h1>
+        <div class="field is-grouped is-grouped-multiline">
+            <div class="control" v-for="tag in tags" :key="tag._id">
+                <div class="tags has-addons" >
+                    <router-link class="tag is-link is-small"
+                                 :to="{name: 'MediaTag', params: {tag: tag._id}}"
+                                 :style="{ fontSize: getFontSize(tag) + 'em' }"
+                                 :title="tag.mediaCount + ' medias'">
+                        {{tag._id}}
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
- import Tags from './Tags.vue'
 
  export default {
      name: 'AllTags',
      props: [],
-     components: {Tags},
      data () {
          return {
              tags: []
          }
      },
+     computed: {
+         maxFontSize() {
+             return Math.max(...this.tags.map(t => t.mediaCount))
+         }
+     },
      created () {
          this.$store.dispatch('getAllTags')
              .then(tags => {
-                 this.tags = tags.map(tag => tag._id)
+                 this.tags = tags.sort((a,b) => a._id > b._id)
              })
+     },
+     methods: {
+         getFontSize(tag) {
+             return .7 + 3 * Math.log(tag.mediaCount) / Math.log(this.maxFontSize)
+         }
      }
  }
 </script>
