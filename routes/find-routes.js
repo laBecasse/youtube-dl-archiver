3c6205f2ecccd1db68cb72ac0eb5adda3287f106
La Bécasse <maxime@buron.coffee>
2020-11-12 01:44:54 +0100

    replace external media by corresponding archive

--- routes/find-routes.js
+++ routes/find-routes.js
@@ -71,49 +71,64 @@ module.exports = function (router, links) {
     } else if (platform === 'sepiasearch') {
       promise = (!offset) ? Sepiasearch.searchMetadataMedias(text) : Promise.resolve([])
     } else {
-      promise = (!offset) ? Downloader.downloadMetadataFromSearch(text, platform)
-        .then(infos => {
-          return infos.map(info => UnarchivedMedia.create(info))
-        }) : Promise.resolve([])
+      promise = (!offset) ? YtDlSearch(text, limit, platform) : Promise.resolve([])
+    }
 
+    if (platform) {
+      promise = promise.then(replaceWithArchive)
     }
 
     return handleJson(promise, req, res)
-
   })
 
   router.get('/medias/:id', (req, res) => {
     const dbId = req.params.id
     handleJson(mediaDB.findById(dbId), req, res)
   })
-}
 
-function emptyAnswerRejection(medias) {
-  return  (medias.length) ? medias : Promise.reject(new Error('empty answer'))
-}
+  function emptyAnswerRejection(medias) {
+    return  (medias.length) ? medias : Promise.reject(new Error('empty answer'))
+  }
 
-function InvidiousSearch(query, limit) {
-  return Invidious.downloadMetadataFromSearch(query).then(infos => infos.map(info => UnarchivedMedia.create(info)))
-}
+  function InvidiousSearch(query, limit) {
+    return Invidious.downloadMetadataFromSearch(query).then(infos => infos.map(info => UnarchivedMedia.create(info)))
+  }
 
-function YtDlSearch(query, limit) {
-  return Downloader.downloadMetadataFromSearch(query, 'youtube')
-    .then(infos => {
-      const medias = infos.map(info => UnarchivedMedia.create(info))
-      return medias
-    })
-}
+  function YtDlSearch(query, limit, platform) {
+    return Downloader.downloadMetadataFromSearch(query, platform)
+      .then(infos => {
+        const medias = infos.map(info => UnarchivedMedia.create(info))
+        return medias
+      })
+  }
 
-function YttSearch(query, limit) {
-  return Ytt.searchMetadataMedias(query, limit)
-    .then(emptyAnswerRejection)
-    .catch(e => {
+  function YttSearch(query, limit) {
+    return Ytt.searchMetadataMedias(query, limit)
+      .then(emptyAnswerRejection)
+      .catch(e => {
         console.warn('Ytt empty answer for query: '+ query)      
-      return InvidiousSeach(query, limit)
-    })
-    .then(emptyAnswerRejection)
-    .catch(e => {
+        return InvidiousSeach(query, limit)
+      })
+      .then(emptyAnswerRejection)
+      .catch(e => {
         console.warn('Invidious empty answer for query: '+ query)      
-      return YtDlSearch(query, limit)
+        return YtDlSearch(query, limit, 'youtube')
+      })
+  }
+
+  function replaceWithArchive(medias) {
+
+    const promises = medias.map(m => {
+      const mediaUrl = m.media_url
+      return mediaDB.findByUrl(mediaUrl)
+        .then(answers => {
+          if (answers.length) {
+            return answers[0]
+          }
+          return m
+        })
     })
+    
+    return Promise.all(promises)
+  }
 }
